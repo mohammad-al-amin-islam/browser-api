414a9c5f2406c90b79868c4d9edb4ba8526d8cc2
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-03-04 08:33:40 +0600

    alert confirm prompt practice added

---
+++ alert-confirm-prompt.js
@@ -0,0 +1,20 @@
+const giveAlert = () =>{
+    alert("al amin don't be lazy");
+}
+
+const getConfirmation = () =>{
+    const response = confirm('Are Ready to Work Hard');
+    if(response === true){
+        alert('Yes he is dedicated');
+    }
+    else{
+        console.log(response,'he is lazy');
+    }
+}
+
+const getInput = () =>{
+    const input = prompt('Are you agree or agree do work hard?');
+    if(input){
+        console.log(input);
+    }
+}
